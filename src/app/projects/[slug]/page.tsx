1b852935a13bf27fcd61dff1068840913c3dfeac
Rokas Kasperavicius <rokaskasperavicius.github@gmail.com>
2024-09-26 11:51:42 +0200

    structured text for projects

--- src/app/projects/[slug]/page.tsx
+++ src/app/projects/[slug]/page.tsx
@@ -1,4 +1,5 @@
 import { DatoImage } from '@/components'
+import { StructuredText } from '@/datocms/components/structured-text'
 
 import { request } from '@/foundation/datocms'
 import { ProjectDocument, ProjectsDocument } from '@/foundation/datocms/types'
@@ -18,22 +19,41 @@ export default async function Page({ params }: { params: { slug: string } }) {
     return null
   }
 
+  const { githubPrimaryUrl, githubSecondaryUrl } = project
+
   return (
     <div className='space-y-8 px-4 py-6 sm:px-4 sm:py-10 md:p-10'>
       <div className='space-y-4'>
-        <h1 className='text-2xl sm:text-3xl md:text-4xl'>{project.title}</h1>
+        <div>
+          <h1 className='text-2xl sm:text-3xl md:text-4xl'>{project.title}</h1>
 
-        {project.githubUrl && (
-          <a
-            target='_blank'
-            className='italic hover:underline sm:text-lg'
-            href={project.githubUrl}
-          >
-            {project.githubUrl}
-          </a>
-        )}
+          <div className='flex flex-col sm:text-lg'>
+            {githubPrimaryUrl && (
+              <a
+                target='_blank'
+                className='italic hover:underline'
+                href={githubPrimaryUrl}
+              >
+                {githubPrimaryUrl}
+              </a>
+            )}
 
-        <p className='sm:text-lg'>{project.description}</p>
+            {githubSecondaryUrl && (
+              <a
+                target='_blank'
+                className='italic hover:underline'
+                href={githubSecondaryUrl}
+              >
+                {githubSecondaryUrl}
+              </a>
+            )}
+          </div>
+        </div>
+
+        <StructuredText
+          content={project.description}
+          className='leading-8 sm:text-xl'
+        />
       </div>
 
       <div className='space-y-4'>
