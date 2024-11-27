9105d9820da5976031cf5af445b3738c5ec4585b
Rokas Kasperavicius <rokaskasperavicius.github@gmail.com>
2024-11-27 22:51:27 +0100

    date condition

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -1,17 +1,22 @@
 export const Footer = async () => {
-  const data = await fetch('https://webhooks.rokaskasperavicius.dev/api/meta')
-  const result = await data.json()
+  let date = null
 
-  const date = new Date(result.date).toLocaleDateString(undefined, {
-    year: 'numeric',
-    month: 'long',
-    day: 'numeric',
-  })
+  try {
+    const data = await fetch('https://webhooks.rokaskasperavicius.dev/api/meta')
+    const result = await data.json()
+
+    date = new Date(result.date).toLocaleDateString(undefined, {
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric',
+    })
+  } catch {}
 
   return (
     <footer className='mt-10 flex flex-row justify-between border-t-2 border-solid border-accent p-4 md:p-6 md:text-xl'>
       <div>Rokas Kasperavicius</div>
-      <div>Last updated at: {date}</div>
+
+      {date && <div>Last updated at: {date}</div>}
     </footer>
   )
 }
