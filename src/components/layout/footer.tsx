0d9e3f53d9d3dac80f1403351ac51efddfeeb90f
Rokas Kasperavicius <rokaskasperavicius.github@gmail.com>
2024-12-01 01:11:11 +0100

    added footer to client components

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -1,16 +1,31 @@
-export const Footer = async () => {
-  let date = null
+'use client'
 
-  try {
-    const data = await fetch('https://webhooks.rokaskasperavicius.dev/api/meta')
-    const result = await data.json()
+import { useEffect, useState } from 'react'
 
-    date = new Date(result.date).toLocaleDateString(undefined, {
-      year: 'numeric',
-      month: 'long',
-      day: 'numeric',
-    })
-  } catch {}
+export const Footer = () => {
+  const [date, setDate] = useState<string | null>(null)
+
+  useEffect(() => {
+    const handle = async () => {
+      try {
+        const data = await fetch(
+          'https://webhooks.rokaskasperavicius.dev/api/meta',
+          { next: { revalidate: 0 } },
+        )
+        const result = await data.json()
+
+        setDate(
+          new Date(result.date).toLocaleDateString(undefined, {
+            year: 'numeric',
+            month: 'long',
+            day: 'numeric',
+          }),
+        )
+      } catch {}
+    }
+
+    handle()
+  }, [])
 
   return (
     <footer className='mt-10 flex flex-row justify-between border-t-2 border-solid border-accent p-4 md:p-6 md:text-xl'>
