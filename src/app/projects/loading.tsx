c2d199e2bdd3fe49c033074eaa50bea27df7a9a7
Rokas Kasperavicius <rokaskasperavicius.github@gmail.com>
2025-05-28 23:36:15 +0200

    feat: testing loading with suspense

---
+++ src/app/projects/loading.tsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+  return (
+    <div className='h-[calc(100dvh-219px)] px-4 py-6 sm:px-4 sm:py-10 md:p-10'>
+      <div className='h-full animate-pulse bg-gray-200'></div>
+    </div>
+  )
+}
